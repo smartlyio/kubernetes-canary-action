894105cd30d8f76ab84101076c90c158f7995e17
Simon Jagoe <simon.jagoe@smartly.io>
2020-08-27 13:31:03 +0300

    Add lock & unlock implementations

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import {isLocked} from './locks'
+import {lock, unlock, isLocked} from './locks'
 
 async function run(): Promise<void> {
   try {
@@ -8,8 +8,13 @@ async function run(): Promise<void> {
     })
     const serviceName = core.getInput('serviceName', {required: true})
     const command = core.getInput('command', {required: true})
+    const user = core.getInput('user') || 'unknown'
     if (command === 'isLocked') {
       await isLocked(kubernetesContext, serviceName)
+    } else if (command === 'lock') {
+      await lock(kubernetesContext, serviceName, user)
+    } else if (command === 'unlock') {
+      await unlock(kubernetesContext, serviceName)
     } else {
       throw new Error(`Command "${command}" is not implemented`)
     }
