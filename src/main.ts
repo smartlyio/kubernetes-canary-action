c7452d36d7c6405dcfd11208f9582afb663b12b7
Simon Jagoe <simon.jagoe@smartly.io>
2020-08-27 16:38:29 +0300

    Add listRecentDeploys command

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import {lock, unlock, isLocked} from './locks'
+import {listRecentDeploys} from './rollback'
 
 async function run(): Promise<void> {
   try {
@@ -7,6 +8,7 @@ async function run(): Promise<void> {
       required: true
     })
     const serviceName = core.getInput('serviceName', {required: true})
+    const deploymentName = core.getInput('deploymentName')
     const command = core.getInput('command', {required: true})
     const user = core.getInput('user') || 'unknown'
     if (command === 'isLocked') {
@@ -15,6 +17,8 @@ async function run(): Promise<void> {
       await lock(kubernetesContext, serviceName, user)
     } else if (command === 'unlock') {
       await unlock(kubernetesContext, serviceName)
+    } else if (command === 'listRecentDeploys') {
+      await listRecentDeploys(kubernetesContext, serviceName, deploymentName)
     } else {
       throw new Error(`Command "${command}" is not implemented`)
     }
